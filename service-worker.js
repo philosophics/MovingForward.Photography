297031d29aa63393111168035292610a73d0f08f
philosophics <philosophicallyintrigued.dl@gmail.com>
2024-12-25 04:17:57 -0700

    service-worker path changes

--- service-worker.js
+++ service-worker.js
@@ -8,22 +8,28 @@ const ASSETS_TO_CACHE = [
   "/MovingForward/assets/images/icon-192x192.png",
   "/MovingForward/assets/images/icon-512x512.png",
   "/MovingForward/assets/pages/about.html",
-  "/MovingForward/assets/pages/abstract.html",
   "/MovingForward/assets/pages/contact.html",
+  "/MovingForward/assets/pages/abstract.html",
   "/MovingForward/assets/pages/landscape.html",
   "/MovingForward/assets/pages/street.html",
 ];
 
-// Install event: Cache app assets
+// Install event
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
+      return Promise.all(
+        ASSETS_TO_CACHE.map((asset) =>
+          cache.add(asset).catch((error) => {
+            console.error(`Failed to cache ${asset}:`, error);
+          })
+        )
+      );
     })
   );
 });
 
-// Activate event: Clean up old caches
+// Activate event
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
@@ -38,7 +44,7 @@ self.addEventListener("activate", (event) => {
   );
 });
 
-// Fetch event: Serve cached assets when offline
+// Fetch event
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
